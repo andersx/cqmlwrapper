45198c00fddc9b8c77dd3201b06de0b0214d9c37
Anders S. Christensen <andersx@nano.ku.dk>
2018-02-22 09:41:38 -0500

    Updated to correct indexing in example

--- cmain.c
+++ cmain.c
@@ -9,13 +9,11 @@ int main() {
     for (i=0; i<2; i++) {
         for (j=0; j<3; j++) {
             coords[i][j] = 3 * i + j;
+            printf("coords() %i %i %f\n", i + 1, j + 1, coords[i][j]);
         }
     }
 
-    printf("%f\n", coords[0][1]);
     print_coords(coords); 
-    
-    printf("%f\n", coords[0][2]);
 
     return 0;
 
